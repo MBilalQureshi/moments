202c4c5314a8de4dce259f60a323beb06ac7d715
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-28 19:24:45 +0000

    Install axios and set defaults

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+// heroku-drf-deployed-project-link
+axios.defaults.baseURL = 'https://moments-m5-c936d2b80f9b.herokuapp.com/'
+// as this is the type of data our API will be expecting, and multipart because there is images other than text
+axios.defaults.headers.post['Content-type'] = 'multipart/form-data'
+// To avoid CORS errors while sending cookies
+axios.defaults.withCredentials = True
