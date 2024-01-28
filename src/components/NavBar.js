f450040338e8bd2d4539f252cfcdccff5b4e2fd5
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-28 17:55:19 +0000

    Apply routes to our navbar links using react router library

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,21 +2,24 @@ import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import logo from '../assets/logo.png'
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     // in normal bootstart fixed-top but in react its fixed=top
     <Navbar className={styles.NavBar} expand="md" fixed='top'>
         <Container>
-            <Navbar.Brand>
-                <img src={logo} alt="logo" height="45" />
-            </Navbar.Brand>
+            <NavLink to='/'>
+                <Navbar.Brand>
+                    <img src={logo} alt="logo" height="45" />
+                </Navbar.Brand>
+            </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <Nav.Link><i className="fa-solid fa-house"></i>Home</Nav.Link>
-                    <Nav.Link><i className="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
-                    <Nav.Link><i className="fa-solid fa-user-plus"></i>Sign up</Nav.Link>
+                    <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to='/'><i className="fa-solid fa-house"></i>Home</NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/signin'><i className="fa-solid fa-right-to-bracket"></i>Sign in</NavLink>
+                    <NavLink className={styles.NavLink} activeClassName={styles.Active} to='/signup'><i className="fa-solid fa-user-plus"></i>Sign up</NavLink>
                 </Nav>
             </Navbar.Collapse>
         </Container>
