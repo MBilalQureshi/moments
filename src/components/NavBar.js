a7f04d2fe79674a93db3905b5459ab135ac1e8c9
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-28 15:37:17 +0000

    Add logo, favicon, font-awesome and update Nav bar style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
+import logo from '../assets/logo.png'
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="lg">
+    // in normal bootstart fixed-top but in react its fixed=top
+    <Navbar expand="md" fixed='top'>
         <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand>
+                <img src={logo} alt="logo" height="45" />
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
+                <Nav className="ml-auto text-left">
+                    <Nav.Link><i className="fa-solid fa-house"></i>Home</Nav.Link>
+                    <Nav.Link><i className="fa-solid fa-right-to-bracket"></i>Sign in</Nav.Link>
+                    <Nav.Link><i className="fa-solid fa-user-plus"></i>Sign up</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
