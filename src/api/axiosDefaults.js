fd7b9b789e737e3f653ac485d5c16510592c2765
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-29 18:36:17 +0000

    Make axios request and response interceptors for acess tokens so that we don't need top login/out

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -6,3 +6,21 @@ axios.defaults.baseURL = 'https://django-rest-framework-m5-2af18e6e1cf9.herokuap
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 // To avoid CORS errors while sending cookies
 axios.defaults.withCredentials = true
+
+
+// Response Interceptor: Our response interceptor will listen for  when the API responds that the user's
+// access token has expired, and then  refresh that token in the background.  
+// This will keep the user  logged in for up to 24 hours.
+export const axiosRes = axios.create();
+
+// Request Interceptor: A request interceptor will automatically intercept  any request our application sends to the API  
+// that requires information about  the logged in user to work.  
+// It will then refresh the user's access  token before sending the request to the API.  
+// This will mean our users can keep  liking posts or following profiles.
+export const axiosReq = axios.create();
+
+// Inside each interceptor we’ll need to write  the logic to refresh the access token.  
+// If our access token has expired, we will  get a 401 error from the server. However,  
+// a 401 just means unauthorized access
+
+// Now go to CurrentUserContext.js to apply all the logic mentioned above all
--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,5 +1,7 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
+import { axiosReq, axiosRes } from '../api/axiosDefaults';
+import { useHistory } from 'react-router-dom';
 
 export const CurrentUserContext = createContext()
 export const SetCurrentUserContext = createContext()
@@ -19,10 +21,13 @@ export const useSetCurrentUser = () => useContext(SetCurrentUserContext)
 export const CurrentUserProvider = ({children}) => {
     const [currentUser, setCurrentUser] = useState(null)
 
+    const history = useHistory()
+
     // request current user during component mounts
     const handleMount = async () => {
       try {
-        const {data} = await axios.get('dj-rest-auth/user/')
+        // let’s  update the axios instance in the handleMount function to the one that actually  uses the response interceptor, namely axiosRes.
+        const {data} = await axiosRes.get('dj-rest-auth/user/')
         setCurrentUser(data)
       } catch(err){
         console.log(err)
@@ -32,6 +37,68 @@ export const CurrentUserProvider = ({children}) => {
     useEffect(() => {
       handleMount()
     },[])
+    
+
+    // useMemo is usually used to cache  complex values that take time to compute.  
+    // The reason we’re using it here is that useMemo  runs before the children components are mounted.  
+    // And we want to attach the interceptors  before the children mount,  
+    // as that’s where we’ll be using  them and making the requests from.
+    useMemo(()=> {
+    // At the moment we don’t have any code  to use our request interceptor with,  
+    // because we aren’t making any requests that need  it yet. But don’t worry, we will be using it soon.
+      // request
+      axiosReq.interceptors.request.use(
+        async (config) => {
+          try {
+            await axios.post("/dj-rest-auth/token/refresh/");
+          } catch (err) {
+            setCurrentUser((prevCurrentUser) => {
+              if (prevCurrentUser) {
+                history.push("/signin");
+              }
+              return null;
+            });
+            return config;
+          }
+          return config;
+        },
+        (err) => {
+          // incase error reject promise with it
+          return Promise.reject(err);
+        }
+      );
+
+      // response
+      axiosRes.interceptors.response.use(
+        // if no error return response
+        (response) => response,
+        // if there is error well check if its 401
+        async(err) => {
+          if(err.response?.status === 401){
+            try{
+              // now try to refresh the access token
+              await axios.post('/dj-rest-auth/token/refresh')
+            } catch(err){
+              // if above refresh fails we'll redirect user to sign in page
+              setCurrentUser(prevCurrentUser => {
+                if(prevCurrentUser){
+                  history.push('/signin')
+                }
+                // and set data to null
+                return null
+              })
+            }
+            // If there’s no error refreshing the token, I’ll return an axios instance with the  error config to exit the interceptor.
+            return axios(err.config)
+          }
+          // In case the error wasn’t 401, I’ll just reject the  Promise with the error to exit the interceptor.
+          return Promise.reject(err)
+        }
+      )
+    // we shouldn’t forget to add a dependency  
+    // array for our useMemo hook with history  inside. We want useMemo to only run once,  
+    // but the linter will throw a warning if  we provided an empty dependency array.
+    },[history])
 
     return(
     // very Context Object comes with  a Provider component that allows child  
