a34b4842c0d0b78be716b6ad8bd8291613266d0e
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-29 15:11:43 +0000

    Add sign in form justconnect with drf API

---
+++ src/pages/auth/SignInForm.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
+
+import { Link, useHistory } from "react-router-dom";
+
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+import axios from "axios";
+
+function SignInForm() {
+
+    const [signInData, setSignInData] = useState({
+        username : '',
+        password : ''
+    })
+    // Destructure username and password from signInData
+    const {username, password} = signInData
+    const history = useHistory();
+    const [errors, setErrors] = useState({})
+    const handleChange = (event) => {
+        setSignInData({
+            ...signInData,
+            [event.target.name] : event.target.value,
+        })
+    }
+    const handleSubmit = async (event) => {
+        //so that the page doesn’t refresh.
+        event.preventDefault()
+        try{
+            await axios.post('/dj-rest-auth/login/',signInData)
+            history.push('/')
+        }
+        catch(err){
+            setErrors(err.response?.data)
+        }
+    }
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto p-0 p-md-2" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>sign in</h1>
+
+          
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+                <Form.Label className="d-none">username</Form.Label>
+                <Form.Control type="text" placeholder="username" name="username" className="styles.Input"
+                onChange={handleChange}
+                value={username} />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>message</Alert>
+            ))}
+
+            <Form.Group controlId="password">
+                <Form.Label className="d-none">Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" name="password" className="styles.Input"
+                onChange={handleChange}
+                value={password} />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+                <Alert variant="warning" key={idx}>message</Alert>
+            ))}
+
+            <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} variant="primary" type="submit">
+                Sign in
+            </Button>
+            {/* Add non_fields_erros like if password does not match */}
+            {errors.non_field_errors?.map((message, idx)=>(
+              <Alert variant="warning" key={idx} className="mt-3">{message}</Alert>
+            ))}
+          </Form>
+
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            Don't have an account? <span>Sign up now!</span>
+          </Link>
+        </Container>
+      </Col>
+      <Col
+        md={6}
+        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
+      >
+        <Image
+          className={`${appStyles.FillerImage}`}
+          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
+        />
+      </Col>
+    </Row>
+  );
+}
+
+export default SignInForm;
