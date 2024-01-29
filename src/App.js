a9b212e4ac80ff4591101c29cfc391b3206608ff
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-29 15:44:33 +0000

    Creatinhg way to fetch curent user value and passing in tree using createContext hook

--- src/App.js
+++ src/App.js
@@ -5,20 +5,52 @@ import { Route,Switch } from 'react-router-dom';
 import './api/axiosDefaults'
 import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const CurrentUserContext = createContext()
+export const setCurrentUserContext = createContext()
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+
+  // request current user during component mounts
+  const handleMount = async () => {
+    try {
+      const {data} = await axios.get('dj-rest-auth/user/')
+      setCurrentUser(data)
+    } catch(err){
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    handleMount()
+  },[])
+
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path='/' render={() => <h1>Home Page</h1>} />
-          <Route exact path='/signin' render={() => <SignInForm/>} />
-          <Route exact path='/signup' render={() => <SignUpForm />} />
-          <Route render={() => <p>Page not found</p>}/>
-        </Switch>
-      </Container>
-    </div>
+    // very Context Object comes with  a Provider component that allows child  
+    // components to subscribe to context changes.
+    // A Provider accepts a value prop to be passed to  child components that need to consume that value.  
+    // In our case, the values being  passed will be the currentUser,  
+    // and setCurrentUser, which is the  function to update the currentUser value.
+    
+    <CurrentUserContext.Provider value={currentUser}>
+      <setCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path='/' render={() => <h1>Home Page</h1>} />
+              <Route exact path='/signin' render={() => <SignInForm/>} />
+              <Route exact path='/signup' render={() => <SignUpForm />} />
+              <Route render={() => <p>Page not found</p>}/>
+            </Switch>
+          </Container>
+        </div>
+      </setCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
