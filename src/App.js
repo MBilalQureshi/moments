15ee7e44b7c3bcb1a70a08d7fa5b1c908d798def
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-02-01 13:51:28 +0000

    Add new home,feed and liked route also add filters and fetch current user

--- src/App.js
+++ src/App.js
@@ -7,15 +7,29 @@ import SignUpForm from './pages/auth/SignUpForm';
 import SignInForm from './pages/auth/SignInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/posts/PostPage';
+import PostsPage from './pages/posts/PostsPage';
+import { useCurrentUser } from './contexts/CurrentUserContext';
 
 function App() {
+  // we’ll create our routes  for our Feed and Liked pages.  
+  // But before we create these routes we’ll  need to know who the currentUser is  
+  // so we can return the posts they liked,  and the ones by profiles they follow.
+  // So, let's set the currentUser value by calling and  auto-importing the useCurrentUser hook. We’ll need  
+  // their profile_id, to know whose profile_id to  filter the posts by. In case the currentUser’s  
+  // details are still being fetched in the  background, it will default to an empty string.
+  const currentUser = useCurrentUser();
+  // learn this below thing
+  const profile_id = currentUser?.profile_id || "";
 
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path='/' render={() => <h1>Home Page</h1>} />
+          {/* as the home page is actually going to show list of posts */}
+          <Route exact path='/' render={() => <PostsPage message="No results found. Ajdust the search keyword." />} />
+          <Route exact path='/feed' render={() => <PostsPage message="No results found. Ajdust the search keyword or follow a user." filter={`owner__followed__owner__profile=${profile_id}&`} />} />
+          <Route exact path='/liked' render={() => <PostsPage message="No results found. Ajdust the search keyword or like a post." filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} />} />
           <Route exact path='/signin' render={() => <SignInForm/>} />
           <Route exact path='/signup' render={() => <SignUpForm />} />
           <Route exact path='/posts/create' render={() => <PostCreateForm />} />
