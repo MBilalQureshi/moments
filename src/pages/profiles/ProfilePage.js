32b2a511c51930d255091cdbe3e27fc44df5e738
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-02-06 20:15:41 +0000

    Set profile data

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -12,14 +12,34 @@ import btnStyles from "../../styles/Button.module.css";
 
 import PopularProfiles from "./PopularProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useParams } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
 
+  // fetch which profile to display id from url
+  const {id} = useParams();
+  const setProfileData = useSetProfileData();
+  // fetch profile data
   useEffect(() => {
-      setHasLoaded(true);
-  }, [])
+    const fetchData = async () => {
+        try{
+            const [{data : pageProfile}] = await Promise.all([axiosReq.get(`/profiles/${id}`)])
+            // setProfileData is part of ProfileDataContext.js
+            setProfileData(prevState => ({
+                ...prevState,
+                pageProfile : {results : [pageProfile]}
+            }))
+            setHasLoaded(true);
+        }catch(err){
+            console.log(err)
+        }
+    }
+    
+  },[id, setProfileData])   
 
   const mainProfile = (
     <>
