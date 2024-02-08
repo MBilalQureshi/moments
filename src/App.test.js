00cd0d0d4b764cddb5173dc103daaf305707a003
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-02-08 17:05:18 +0000

    Setting up our testing in react enviornment and setting up its handlers.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,35 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //https://testing-library.com/docs/queries/about/
+  //  First, we need to render the component we’d like to test.
+  //render(<App />);
+
+  /*  Then, we’ll have to target specific elements on the screen, using an appropriate query method.
+  Optionally, we could interact with the returned elements,
+  for example simulate a user click, depending on how sophisticated we’d like our test to be*/
+  //const linkElement = screen.getByText(/learn react/i);
+
+  //  Finally, we can make our assertions
+  //expect(linkElement).toBeInTheDocument();
+   //------------------------------
+  /**
+   * https://testing-library.com/docs/react-testing-library/intro/
+   *  there are quite a few drawbacks to this approach:
+      Requests are slow. Requests can occasionally cause errors even when
+      our code is fine. So it’s not ideal for the tests to depend on an external resource such as an API.
+      Requests can be costly. When you come to work in a professional environment you’re
+      likely to work with paid for 3rd party APIs. In this case, making constant API requests to
+      test code may become very expensive. How do we go about doing that then?
+      We can do it by creating mock API responses using request handlers
+      to interact with our components and functions. To do this, the React testing library recommends
+      using a library called Mock Service Worker, which uses an API built into
+      all modern browsers to intercept requests so that we can declaratively mock API responses.
+      So let’s use it to create the two mock endpoints we need to test our NavBar component.
+
+      npm install msw --save-dev
+      only need it for testing, we’ll use the --save-dev tag at the end of the command. This way,I’ll have
+      it saved as a dependency which we only use in the development version of our project.
+   */
+
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,20 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// we’ll first import the setupServer function from the msw library.
+import {setupServer} from 'msw/node'
+
+// Then we’ll create our server by calling the setupServer function we just imported.
+// We’ll call it with the spread handlers array that we’ll also auto-import into the file.
+// Now we can move onto the test setup and teardown.
+const server = setupServer (...handlers)
+
+// We’ll first call the server’s listen method before all the tests
+beforeAll(() => server.listen())
+
+// call resetHandlers
+afterEach(()=> server.resetHandlers())
+
+//after each test and finally shut the server down after all the tests have been run.
+afterAll(()=> server.close)
